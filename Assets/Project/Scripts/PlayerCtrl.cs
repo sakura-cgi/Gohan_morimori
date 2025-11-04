defd217b95c6d996caa879fbb29c348df1836ba3
sakupan39 <im0250hs@ed.ritsumei.ac.jp>
2025-11-04 18:09:03 +0900

    キャラクターのビジュアル改善

--- Assets/Project/Scripts/PlayerCtrl.cs
+++ Assets/Project/Scripts/PlayerCtrl.cs
@@ -1,41 +1,41 @@
 using UnityEngine;
 
-// [RequireComponent(typeof(Rigidbody2D))]
-// public class PlayerMovement : MonoBehaviour
-// {
-//     [SerializeField] float moveSpeed = 5f;
-//     [SerializeField] float jumpForce = 7f;
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpForce = 7f;
 
-//     private Rigidbody2D rb;
-//     private bool isGrounded;
-//     private float moveInput;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private float moveInput;
 
-//     void Start() => rb = GetComponent<Rigidbody2D>();
+    void Start() => rb = GetComponent<Rigidbody2D>();
 
-//     void Update()
-//     {
-//         moveInput = Input.GetAxisRaw("Horizontal");
-//         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-//             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-//     }
+    void Update()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
 
-//     void FixedUpdate()
-//     {
-//         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
-//     }
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+    }
 
-//     void OnCollisionStay2D(Collision2D collision)
-//     {
-//         if (collision.collider.CompareTag("Ground"))
-//             isGrounded = true;
-//     }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+            isGrounded = true;
+    }
 
-//     void OnCollisionExit2D(Collision2D collision)
-//     {
-//         if (collision.collider.CompareTag("Ground"))
-//             isGrounded = false;
-//     }
-// }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+            isGrounded = false;
+    }
+}
 
 
 // using UnityEngine;

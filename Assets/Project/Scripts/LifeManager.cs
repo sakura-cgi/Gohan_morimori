6b7f6b5784d05655cbd77ccd36a743546622a8e4
N-Sakura01 <im0257ri@ed.ritsumei.ac.jp>
2025-11-07 18:35:46 +0900

    ライフマネージャー作ったよ

--- Assets/Project/Scripts/LifeManager.cs
+++ Assets/Project/Scripts/LifeManager.cs
@@ -3,20 +3,26 @@ using UnityEngine.UI;
 
 public class LifeManager : MonoBehaviour
 {
-    [SerializeField] private Image[] LifeImage;
+    [SerializeField] private Image LifeImage;
+     [SerializeField] private RectTransform canvas;
     public int life = 5;
     void Start()
     {
+         for (int i = 0; i < life; i++)
+        {
+            Image newLife = Instantiate(LifeImage,canvas);
+            RectTransform rect = newLife.GetComponent<RectTransform>();
+            rect.anchorMin = rect.anchorMax = new Vector2(0.5f, 0.5f);
+            rect.pivot = new Vector2(0.5f, 0.5f);
+            rect.anchoredPosition = new Vector2(i * 40 - 370, 130);
+
+        }
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < life; i++)
-        {
-            LifeImage[i].enabled = true;
-            ///rect.localPosition += new Vector3(10, 0, 0);
-        }
+
     }
 }

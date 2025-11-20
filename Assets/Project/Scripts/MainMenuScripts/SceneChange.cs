006af0b62882fb5a31e01e232908c305378f8c21
N-Sakura01 <im0257ri@ed.ritsumei.ac.jp>
2025-11-20 10:52:05 +0900

    タイトルとポーズメニューの仮作成

---
+++ Assets/Project/Scripts/MainMenuScripts/SceneChange.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using NUnit.Framework;
+
+public class SceneChange : MonoBehaviour,ActionBase
+{
+    [SerializeField] string sceneName;  
+    public void Action()
+    {
+        Debug.Log("SceneChange");
+        ///SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/Project/Scripts/PauseMenuScripts/PauseScript.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject pauseMenuUI; 
+    [SerializeField] private Button resumeButton;     
+    [SerializeField] private Button quitButton;
+     private bool isPaused = false;
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+        resumeButton.onClick.AddListener(ResumeGame);
+        ///quitButton.onClick.AddListener(QuitGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ChangePause();
+        }
+        
+    }
+
+    void ChangePause()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+     private void PauseGame()
+    {
+        // ゲーム時間を停止
+        Time.timeScale = 0f;
+
+        // UIを表示
+        pauseMenuUI.SetActive(true);
+
+        // ポーズ状態を更新
+        isPaused = true;
+    }
+    private void ResumeGame()
+    {
+        // ゲーム時間を再開
+        Time.timeScale = 1f;
+
+        // UIを非表示
+        pauseMenuUI.SetActive(false);
+
+        // ポーズ状態を更新
+        isPaused = false;
+    }
+}
